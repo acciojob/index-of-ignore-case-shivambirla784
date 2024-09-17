cd7818e1a80e4244db001e0206df8f968dcc071f
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-09-18 00:54:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerStr = s1.toLowerCase();
+  const lowerSubStr = s2.toLowerCase();
+
+  // Use indexOf to find the first occurrence of lowerSubStr in lowerStr
+  return lowerStr.indexOf(lowerSubStr);
 }
 
 // Please do not change the code below
